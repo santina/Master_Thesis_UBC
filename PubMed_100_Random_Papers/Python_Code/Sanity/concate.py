346f5d18df6127cb2e3251116bdd5a05fc62cedf
Santina Lin <santina424@gmail.com>
2016-11-12 16:08:39 -0800

    Make comment about the file more clear

--- PubMed_100_Random_Papers/Python_Code/Sanity/concate.py
+++ PubMed_100_Random_Papers/Python_Code/Sanity/concate.py
@@ -1,8 +1,6 @@
-# Combine the files in /projects/slin_prj/PubMed_Experiment/categories 
-# each file:  PMID Title AbstractText 
-# Written in Python 3 
+# Concatenate files that contain abstracts in each line and exclude duplicates at the same time.
 
-import argparse 
+import argparse
 from os import listdir
 from os.path import isfile, isdir, join
 from sets import Set
@@ -11,12 +9,12 @@ def concate(abstractFolder, outFileObject):
 	''' Combine files content by copying the content in `abstractFolder` to `outFileObject
 	'''
 
-	idHash = Set([])
+	idHash = Set([]) # to keep track of duplicates
 	for abstractfile in [f for f in listdir(abstractFolder) if isfile(join(abstractFolder, f))]:
 		curFile = join(abstractFolder, abstractfile)
-		
 
-		# Open the file and write the needed content to the outfile object 
+
+		# Open the file and write the needed content to the outfile object
 		with open(curFile) as f:
 			for lineNum, line in enumerate(f):
 				line = line.strip().split('\t')
@@ -24,10 +22,10 @@ def concate(abstractFolder, outFileObject):
 					pubmedID, title, abstract = line[0], line[1], line[2]
 				except IndexError:
 					print curFile
-					break # Go to the next file 
-				if int(pubmedID) not in idHash: 
+					break # Go to the next file
+				if int(pubmedID) not in idHash:
 					idHash.add(int(pubmedID))
-					outString = '\t'.join([abstractfile, pubmedID, title, abstract])  # filename, pubmedID, title, abstract 
+					outString = '\t'.join([abstractfile, pubmedID, title, abstract])  # filename, pubmedID, title, abstract
 					outFileObject.write(outString + '\n')
 
 
@@ -35,8 +33,6 @@ def main():
 	parser = argparse.ArgumentParser(description='')
 	parser.add_argument('--f', type=str, help='Folder with all the files')
 	parser.add_argument('--out', type=str, help='File to write the result to')
-
-
 
 	args = parser.parse_args()
 	out = open(args.out, 'w')
