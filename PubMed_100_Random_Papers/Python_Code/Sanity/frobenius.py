58c31dbd8952b453f90f5cadc0e6e6ddc666b001
Santina Lin <santina424@gmail.com>
2016-11-12 16:08:56 -0800

    Allow reading sv with different files

--- PubMed_100_Random_Papers/Python_Code/Sanity/frobenius.py
+++ PubMed_100_Random_Papers/Python_Code/Sanity/frobenius.py
@@ -1,18 +1,35 @@
-# Evaluate number of singular values to keep 
-# input file is the log file from GraphLab, we want to see the variance coverage on the choice of nsv 
+# Evaluate number of singular values to keep
+# input file is the log file from GraphLab, we want to see the variance coverage on the choice of nsv
 
 import numpy as np
 import random
 import argparse
 
+def main():
+	args = parse_arguments()
+	frob = frobenius(args.m)
+	sv = get_singular_values(args.s, args.n)
+	print "Forbenius: " + str(frob)
+	variance = getSingularVariance(sv)
+	print "Variance: " + str(variance)
+	print "Coverage: " + str(variance/frob)
 
-# Get variance of the input matrix 
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='')
+	parser.add_argument('--log', type=str, help='Log file from running GraphLab. It contains singular values and their errors.')
+	parser.add_argument('--s', type=str, help="singular value file")
+	parser.add_argument('--m', type=str, help='Matrix file')
+	parser.add_argument('--n', type=int, help='Number of singular values')
+
+	return parser.parse_args()
+
+# Get variance of the input matrix
 def getMatrixVariance(matrix):
 	variance = 0
 	for r in range(0, matrix.shape[0]):
 		for c in range(0, matrix.shape[1]):
 			variance += matrix[r][c]**2
-	return variance 
+	return variance
 
 # Get variance of the first 'nranks' of the singular value array
 def getSingularVariance(s, nranks=None):
@@ -24,27 +41,27 @@ def getSingularVariance(s, nranks=None):
 	for i in range(0, nranks):
 		variance += s[i]**2
 
-	return variance 
+	return variance
 
-# Input : same matrix file to GraphLab 
+# Input : same matrix file to GraphLab
 def frobenius(matrixfile):
 	frobenius = 0
 
 	with open(matrixfile) as f:
-		for _, line in enumerate(f): # 0 base for lineNum 
+		for _, line in enumerate(f): # 0 base for lineNum
 			_, _, val = line.strip().split('\t')
 			frobenius += float(val)**2
 	return frobenius
 
 
-# Read in the log file to get an array singluar value and an array of errors 
-def getSingularValues(logfile_path, nvalues):
-	singularVals = [] 
-	errors = []  
+# Read in the log file to get an array singluar value and an array of errors
+def get_singular_values_from_log(logfile_path, nvalues):
+	singularVals = []
+	errors = []
 	count = 0
 
 	with open(logfile_path) as f:
-		for _, line in enumerate(f): # 0 base for lineNum 
+		for _, line in enumerate(f): # 0 base for lineNum
 			if "Singular value" in line:
 				content = line.split()
 				singularVals.append(float(content[3]))
@@ -52,10 +69,25 @@ def getSingularValues(logfile_path, nvalues):
 				count += 1
 				if count == nvalues:
 					break
-			
+
 
 	assert len(errors) == len(singularVals)
 	return singularVals, errors
+
+def get_singular_values(singular_val_file, nvalues):
+	singularVals = []
+	nsv = 0
+	with open(singular_val_file) as f:
+		for line in f:
+			if nsv >= nvalues:
+				break
+			try:
+				singularVals.append(float(line))
+				nsv += 1
+			except:
+				pass # omid the first line of the file, descriptor of the file
+
+	return singularVals
 
 
 def findIndexWLowerError(array, start=0):
@@ -64,32 +96,5 @@ def findIndexWLowerError(array, start=0):
 			return i
 
 
-if __name__ == "__main__":
-
-	parser = argparse.ArgumentParser(description='')
-	parser.add_argument('--s', type=str, help='Log file running GraphLab. It contains singular values and their errors.')
-	parser.add_argument('--m', type=str, help='Matrix file')
-	parser.add_argument('--n', type=int, help='Number of singular values')
-	args = parser.parse_args()	
-	
-	s, e = getSingularValues(args.s, args.n)
-	frob = frobenius(args.m)
-	print frob
-
-	index = findIndexWLowerError(e, 1)
-	print index  # So number of nsv is index + 1 
-	print e[index-1]
-	print e[index]
-	print e[index+1]
-	variance = getSingularVariance(s, index)
-	print variance
-	print variance/frob
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+	main()
