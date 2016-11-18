ce8c5c2246a00f5cada2bae58330e3813f8b03bc
Santina Lin <santina424@gmail.com>
2016-11-18 14:37:05 -0800

    Add additional functionality
    - Create put request
    - Update a page information
    - Add questions

--- Surveys_Creation/api_service/basic_service.py
+++ Surveys_Creation/api_service/basic_service.py
@@ -1,4 +1,5 @@
-import requests # > 2.12.0
+from . import questions
+import requests
 
 # Survey Monkey host.
 HOST = "https://api.surveymonkey.net/v3"
@@ -22,6 +23,15 @@ class ApiService(object):
         response = None
         for i in range(0,MAX_ATTEMPTS):
             response = self.client.post(request_url, json=payload)
+            if response:
+                break
+
+        return response.json()
+
+    def make_put_request(self, request_url, payload):
+        response = None
+        for i in range(0,MAX_ATTEMPTS):
+            response = self.client.put(request_url, json=payload)
             if response:
                 break
 
@@ -71,6 +81,11 @@ class ApiService(object):
         url = HOST + "/surveys/%s/pages" %(survey_id)
         return self.make_get_request(url)
 
+    def get_page_info(self, survey_id, page_id):
+        """ Obtain information for one specific page in a survey """
+        url = HOST + "/surveys/%s/pages/%s" %(survey_id, page_id)
+        return self.make_get_request(url)
+
     def get_pages_ids(self, survey_id):
         """ Return the ids of the pages in the survey """
         pages_info = self.get_pages_info(survey_id)
@@ -79,16 +94,37 @@ class ApiService(object):
             page_ids.append(page["id"])
         return page_ids
 
-    def add_page_description(self, survey_id, page_id, paragraph):
-        """ Add description of the page """
-        pass
+    def create_new_page(self, survey_id, title, description):
+        """ Create a new page with title and description of the page,
+        and return the page id """
+        url = HOST + "/surveys/%s/pages" % (survey_id)
+        payload = {
+          "title": title,
+          "description" : description
+        }
+        response = self.make_post_request(url, payload)
+        return response
 
-    def add_paragraph(self, survey_id, page_id, abstract):
-        """ Add a block of text to the page """
+    def update_title_description_of_page(self, survey_id, page_id, title, description):
+        """ Modify the title and description of a page in a survey. """
+        url = HOST + "/surveys/%s/pages/%s" %(survey_id, page_id)
+        payload = {
+          "title": title,
+          "description" : description
+        }
+        response = self.make_put_request(url, payload)
+        return response
+
+    def add_paragraph(self, survey_id, page_id, text):
+        """ Add a block of text to the page. """
         url = HOST + "/surveys/%s/pages/%s/questions" % (survey_id, page_id)
+        text_data = questions.make_paragraph(text)
+        response = self.make_post_request(url, text_data)
+        return response
 
-        pass
-
-    def add_question(self, survey_id, page_id, question):
-        """ Add a question to the page """ 
-        pass
+    def add_single_choice(self, survey_id, page_id, title, choices):
+        """ Add a single_choice question to the page """
+        url = HOST + "/surveys/%s/pages/%s/questions" % (survey_id, page_id)
+        question_data = questions.make_single_choice_question(title, choices)
+        response = self.make_post_request(url, question_data)
+        return response
