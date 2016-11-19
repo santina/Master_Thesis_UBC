73712768ce224f2515be17a17ed8dcc12176c10f
Santina Lin <santina424@gmail.com>
2016-11-18 16:07:16 -0800

    clean up example usage

--- Surveys_Creation/create_evaluation_survey.py
+++ Surveys_Creation/create_evaluation_survey.py
@@ -4,31 +4,24 @@ from private import appInfo
 
 loremipsum = "Lorem ipsum dolor sit amet, consecteteur adipiscing elit donec proin nulla vivamus. Augue donec a erat ve sagittis nisi rhoncus curabitur mauris. Nulla ipsum tortor sagittis adipiscing primis interdum suspendisse lobortis etiam risus nullam. Donec massa quam dis at nibh dolor netus quis. Purus etiam. Dolor neque nunc netus eget nulla faucibus vestibulum aenean class senectus. Porta dolor. Donec morbi. Felis lorem tempus luctus malesuada laoreet curae justo rhoncus ante facilisi parturient malesuada elit laoreet amet. Fusce augue nisi ligula praesent condimentum nascetur fringilla in id lectus per nunc. Lacus metus nisl orci odio maecenas adipiscing. Velit nulla a tempor class placerat ac condimentum nisi taciti at eros."
 
+loremipsum_A = "A: \n" + loremipsum
+loremipsum_B = "B: \n" + loremipsum
+
 client = basic_service.ApiService(appInfo.ACCESS_TOKEN)
 
-#Create a new survey
-# survey_id = client.create_survey("Evaluation_template")
-# print(survey_id)
-# # Create five pages of survey
-# for i in range(0, 2):
-#     print(client.create_new_page(survey_id, str(i), loremipsum)) # title and description
-#     #print r.json()
-#     print
-# # Get the page ids
-# ids = client.get_pages_ids(survey_id)
-#
-# print(ids)
+# Create a new survey
+survey_id = client.create_survey("Evaluation_template")
+# Create five pages of survey
+for i in range(0, 2):
+    client.create_new_page(survey_id, str(i), loremipsum) # title and description
 
-page_ids = ["26984311", "26984312", "26984314"]
-survey_id = "111307043"
+# Get the page ids
+page_ids = client.get_pages_ids(survey_id) # There will be 3 
+
 answers = ["A", "B"]
-question_title = "Which is more relevant?"
-for ID in page_ids:
-#    print(client.get_page_info(survey_id, ID))
-#    print(client.update_title_description_of_page(survey_id, ID, "TITLE", loremipsum))
-    print(client.add_single_choice(survey_id, ID, question_title, answers))
-    print(client.add_paragraph(survey_id, ID, loremipsum))
-    print(client.add_paragraph(survey_id, ID, loremipsum))
-
-print
-print(client.get_pages_info(survey_id))
+question_title = "Which of the following abstract is more relevant to the one above?"
+for i, ID in enumerate(page_ids):
+    client.update_title_description_of_page(survey_id, ID, "Abstract" + str(i), loremipsum)
+    client.add_single_choice(survey_id, ID, question_title, answers)
+    client.add_paragraph(survey_id, ID, loremipsum_A)
+    client.add_paragraph(survey_id, ID, loremipsum_B)
