db16f81a96355882d213313509259e54b68ed352
Santina Lin <santina424@gmail.com>
2016-12-20 15:14:27 -0800

    Fix frobenius norm mistake
    - Fix calculation in `calculate_coverage.py`
    - Rerun the python code and generate new data
    - Update the graphs on frobenius norm

--- Graph_Sparsification/Code/calculate_coverage.py
+++ Graph_Sparsification/Code/calculate_coverage.py
@@ -1,24 +1,21 @@
+# Generate a R data.frame friendly text file with two columns : nsv and coverage
+
 import sys
 import re
 import timeit
 import argparse
 from collections import defaultdict
-
-"""
-    Generate a R data.frame friendly text file with two columns : nsv and coverage
-"""
+import math
 
 def main():
-    # Parse and sanity check the inputs
     args = parse_args()
-
-    record_coverage(args.m, args.s, args.min, args.max, args.step, args.out)
+    record_coverage(args.m, args.log, args.min, args.max, args.step, args.out)
 
 def parse_args():
     """ Parsing input arguments """
-    parser = argparse.ArgumentParser(description="Calculate forbenius norms on a folder of Graphlab result folders")
-    parser.add_argument('--m', type=str, help="folder with the matrices")
-    parser.add_argument('--s', type=str, help="folder with the SVD result folders")
+    parser = argparse.ArgumentParser(description="Calculate frobenius norms on a folder of Graphlab result folders")
+    parser.add_argument('--m', type=str, help="matrix file")
+    parser.add_argument('--log', type=str, help="GraphLab's log file")
     parser.add_argument('--out', type=str, help="Out file to write the results to")
     parser.add_argument('--max', type=int, help="The maximum number of singular values")
     parser.add_argument('--min', type=int, help="The minimum number of singular values")
@@ -32,7 +29,7 @@ def parse_args():
     return args
 
 def record_coverage(matrix, svals, min_nsv, max_nsv, step, out):
-    print(str(min_nsv) + " " + str(max_nsv) + " " + str(step))
+    """ Write the coverage at different nsv into a file """
     frobenius_norm = frobenius(matrix)
     print("Done with the big matrix")
     print(str(frobenius_norm))
@@ -46,26 +43,20 @@ def record_coverage(matrix, svals, min_nsv, max_nsv, step, out):
         for i in range(min_nsv, max_nsv+1, step):
             print(i)
             variance = sum([s**2 for s in singular_values[0:i+1]])
-            coverage = float(variance)/frobenius_norm
+            variance = math.sqrt(variance)
+            coverage = variance/frobenius_norm
             outfile.write(str(i) + "\t" + str(coverage) + "\t" + str(variance) +  "\n")
 
-
-
-
-
 def frobenius(matrixfile):
-    """ Given the same matrix file to GraphLab, return the forbenius norm.
+    """ Given the same matrix file to GraphLab, return the frobenius norm.
     """
     frobenius = 0
     with open(matrixfile) as f:
     	for _, line in enumerate(f): # 0 base for lineNum
     		_, _, val = line.strip().split('\t')
     		frobenius += float(val)**2
+    frobenius = math.sqrt(frobenius)
     return frobenius
-
-def calculateVariances(svals, start_nsv, end_nsv):
-    return sum([s**2 for s in svals[start_nsv:end_nsv+1]])
-
 
 def getSingularValues(logfile_path, nvalues):
     """ Read in the log file to get an array singluar value and an array of errors
@@ -83,8 +74,6 @@ def getSingularValues(logfile_path, nvalues):
     				break
 
 	return singularVals
-
-
 
 if __name__ == '__main__':
     main()
